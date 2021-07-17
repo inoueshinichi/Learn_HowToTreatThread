60a5d7fd0f0a39969eea7bd837bbdf521147309a
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-07-17 18:49:57 +0900

    update

---
+++ cpp_standard_thread.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file cpp_standard_thread.cpp
+ * @author your name (you@domain.com)
+ * @brief C++11以降で実行できるスレッドライブラリ
+ * @version 0.1
+ * @date 2021-07-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+
+
+int main(int, char **)
+{
+    const int N = 100000;
+    for (int i = 1; i < 10; ++i)
+        multi(M, i);
+}
