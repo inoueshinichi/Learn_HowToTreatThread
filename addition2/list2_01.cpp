656838b757a1d7e60b92c86c9339e0076ebc31d2
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-10-18 15:24:04 +0900

    mutexによる排他制御追加

--- addition2/list2_01.cpp
+++ addition2/list2_01.cpp
@@ -10,12 +10,28 @@ void add(long* address, long value) {
   *address += value;
 }
 
+
+// mutexを用いたお利口さんな足し算
+std::mutex mtx;
+void mutex_add(long* address, long value) {
+  mtx.lock();
+  *address += value;
+  mtx.unlock();
+}
+
+/**
+ * @brief クリティカルセクションでの例外処理
+ * lock()とunlock()で挟まれた処理の中で例外がthrowされると困ったことになります。
+ * 例外で処理が中断するとunlock()に達しないのでロックが解放されず、他のスレッドが動けなくなっちゃいます。
+ * → std::lock_guardまたはstd::unique_lock を使う.
+ */
+
 void run(std::function<void(long*, long)> fun, long* address) {
     using namespace std;
     using namespace std::chrono;
 
     auto task = [=](int n, long v) { while (n--) fun(address, v); };
-    const int N = 100000;
+    const int N = 100000; // 10万
     thread threads[4];
     auto start = high_resolution_clock::now();
 
@@ -40,6 +56,11 @@ int main(int, char**) {
     cout << "data-race:            ";
     count = 0; 
     run(add, &count); 
+    cout << ", count = " << count << endl;
+
+    cout << "critical-section:     ";
+    count = 0;
+    run(mutex_add, &count);
     cout << ", count = " << count << endl;
     
     return 0;
